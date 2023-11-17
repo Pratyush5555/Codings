bf0433ed3b64b40f74d883a1a00032f8422d5e65
Pratyush555 <143861572+Pratyush5555@users.noreply.github.com>
2023-11-17 10:58:09 +0530

    Add files via upload

---
+++ JAVA coding/Coding/src/com/org/java/ForLoops.java
@@ -0,0 +1,13 @@
+package com.org.java;
+
+public class ForLoops {
+   public static void main(String[] args) { 
+	  
+	   for(int i = 0;i< 5; i++) {
+		   //System.out.println("hello");
+		  // System.out.println("The value of i is :" + i);
+		  // System.out.printf("The value of i is :%d " , i);/*print on a single line.*/
+		     System.out.printf("The value of i is :%d\n " , i);// in diff line 
+	   }
+  }
+}
---
+++ JAVA coding/Coding/src/com/org/java/Strings.java
@@ -0,0 +1,15 @@
+package com.org.java;
+
+public class Strings {
+  public static void main(String[] args) {
+	String text = "hello";
+	String blank =  " ";
+	String name = "Ram";
+	String mention = text + blank + name; 
+	System.out.println("hello"+ " "+ "Ram");
+	//another type
+	
+	System.out.println(mention);
+
+}
+}
